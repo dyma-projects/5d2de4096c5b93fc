597a1fe65770fec96624414153c19af21bdebf4e
Florence Colas <colasflorence8@gmail.com>
2019-07-24 18:02:53 +0200

    projet 5

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+	public users: BehaviorSubject<string[]> = new BehaviorSubject([]);
+
+  constructor() { }
+
+  addUser(name: string): void {
+  	this.users.value.push(name);
+    this.users.next(this.users.value);
+  }
+}
